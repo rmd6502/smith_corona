ab3ad149390e40ec16779851f9a3579ed676b41e
Robert Diamond <rmd6502@gmail.com>
2014-03-09 00:02:01 -0500

    latest

--- smith_corona/smith_corona.ino
+++ smith_corona/smith_corona.ino
@@ -14,7 +14,7 @@ String codeKeys[] = {
   "Tab C","^","ç","é","`","¿","~",
   "[","]","D.Tab","p","§","Reloc","j",
   "ShiftLock","a","s","d","f","g","h",
-  "k","l",";","'","m",",",".",
+  "k","l",";","¶","m",",",".",
   "LShift","Demo","x","c","v","Bold","n",
   "RShift","/","Code","Margin R","Half Space","Word Erase","Correct"
 };
@@ -30,36 +30,44 @@ String shiftKeys[] = {
 };
 
 uint8_t rows[] = {
-  A2,A3,A1,A4,A0,A5,3,12};
+  A2,A3,A1,A4,A0,A5,3,12
+};
+
+// pullups are on the columns
 uint8_t cols[] = {
-  6,7,8,9,10,11,13};
+  6,7,8,9,10,11,13
+};
 
 #define NUM_ROWS (sizeof(rows))
 #define NUM_COLS (sizeof(cols))
+
+uint8_t state = 0;
 
 void setup() {
   for (uint8_t i=0; i < NUM_ROWS; ++i) {
     pinMode(rows[i], INPUT);
   }
   for (uint8_t i=0; i < NUM_COLS; ++i) {
-    pinMode(cols[i], INPUT);
+    digitalWrite(cols[i],HIGH);
+    pinMode(cols[i], OUTPUT);
+    digitalWrite(cols[i],HIGH);
   }
   Serial.begin(9600);
 }
 
 void loop() {
 
-  uint16_t i,j;
-  for (i=0; i < NUM_COLS; ++i) {
-    digitalWrite(cols[i], LOW);
-    for (j=0; j < NUM_ROWS; ++j) {
-      if (digitalRead(rows[j]) == LOW) {
-        Serial.print(i); 
-        Serial.print("  "); 
-        Serial.println(j);
-      }
+  uint16_t i,j,b=1,val=0;
+  for (i=0; i < NUM_ROWS; ++i) {
+    if (digitalRead(rows[i]) == LOW) {
+      val |= b;
     }
-    digitalWrite(cols[i], HIGH);
+    b <<= 1;
+  }
+  if (val & 1) {
+    digitalWrite(cols[2],LOW);
+  } else {
+    digitalWrite(cols[2],HIGH);
   }
 }
 
